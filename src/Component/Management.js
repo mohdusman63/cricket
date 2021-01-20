d1f8abe00185cf47907fe0e3b8fa81bfa2d7d6e5
chandraprakashrajpoot <mohdusmanali63@gmail.com>
2021-01-20 18:33:17 +0530

    react multiselect added

--- src/Component/Management.js
+++ src/Component/Management.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import MultiSelect from "react-multi-select-component";
 
 function Management(props) {
   let [teamList, setTeamList] = useState([]); //add list of team
   let [playerList, setPlayerList] = useState([]); //add all the team player
   let [loading, setLoading] = useState("loading....");
-  let [team1, setTeam1] = useState(""); //store the team1 members
-  let [team2, setTeam2] = useState(""); //store the team2 members
+  let [team1, setTeam1] = useState([]); //store the team1 members
+  let [team2, setTeam2] = useState([]); //store the team2 members
   let [id, setIds] = useState([]);
+  let [teamValue, setTeamValue] = useState([]); //dropdown list of player team1
+  let [teamValue2, setTeamValue2] = useState([]); //dropdown list of player team2
+  const [selected, setSelected] = useState([]); //select the two batsman
+  const [selected2, setSelected2] = useState([]); //select the bowler
   useEffect(() => {
     async function getTeam() {
       try {
@@ -54,6 +59,7 @@ function Management(props) {
       alert("you can perform match maximum two team ");
       return false;
     }
+
     const { checked, value } = e.target;
     setIds((preValue) => {
       return [...preValue, value];
@@ -61,11 +67,23 @@ function Management(props) {
     getList(value);
   };
   const getPlayer = () => {
-    console.log(playerList[0]);
+    console.log(playerList);
     setTeam1(playerList[0]);
     console.log("");
     setTeam2(playerList[1]);
-    console.log(playerList[1]);
+    //console.log(playerList[0]);
+    //console.log(playerList[1]);
+    playerList[0].map((elel) =>
+      setTeamValue((pre) => {
+        return [...pre, { label: elel.name, value: elel.player_id }];
+      })
+    );
+    playerList[1].map((elel1) =>
+      setTeamValue2((prev) => {
+        return [...prev, { label: elel1.name, value: elel1.player_id }];
+      })
+    );
+    //teamValue;
   };
   const handler = (e) => {
     console.log(e.target);
@@ -107,17 +125,37 @@ function Management(props) {
         </div>
       </div>
       <button className="btn btn-success" onClick={() => getPlayer()}>
-        click me
+        Start Match
       </button>
+
+      {/* player list of both team */}
+      <div className=" row m-4 ">
+        <h6>Select Player </h6>
+
+        <MultiSelect
+          className="m-4"
+          options={teamValue}
+          value={selected}
+          onChange={setSelected}
+          labelledBy={"Select"}
+        />
+        <MultiSelect
+          className="m-4"
+          options={teamValue2}
+          value={selected2}
+          onChange={setSelected2}
+          labelledBy={"Select"}
+        />
+      </div>
       {/* listing of the player name */}
-      <div className="row m-2">
+      {/* <div className="row m-2">
         <select className="m-2" multiple={true} onChange={handler}>
           <option value="" disabled>
             Player Name
           </option>
           {team1
             ? team1.map((val, index) => (
-                <option key={index} value={val.player_id} text={val.name}>
+                <option key={index} value={val.player_id} name={val.name}>
                   {val.name}{" "}
                 </option>
               ))
@@ -138,7 +176,7 @@ function Management(props) {
               ))
             : " "}
         </select>
-      </div>
+      </div> */}
     </>
   );
 }
